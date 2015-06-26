3435dee607080217a7933a5eb8633727da59fa73
blackout314 <blackout_lair@yahoo.com>
2015-06-26 11:01:16 +0200

    add btc ticker plugin

---
+++ bin/plugins/btc.js
@@ -0,0 +1,41 @@
+var request = require('request');
+
+var exec = function (param, cb) {
+  var error,
+  name = "Bitcoin Ticker Plugin";
+
+  if(param) {
+    var APIURL = "https://blockchain.info/it/ticker";
+    request({
+      method: 'GET',
+      url: APIURL,
+      json: true
+    }, function (err, resp, body) {
+      if (!err) {
+        if(resp.statusCode === 200 && typeof body[param] !== "undefined") {
+          var ticker = body[param];
+          return cb({
+            currency: ticker.symbol,
+            exchange: ticker.last,
+            buy: ticker.buy,
+            sell: ticker.sell
+          });
+        } else {
+          return cb({
+            error: "Currency not found"
+          });
+        }
+      }
+
+      error = new Error("Cannot retrieve information about choosed currency");
+      error.name = name;
+      throw error;
+    });
+  } else {
+    error = new Error("You must specify which currency you want to check");
+    error.name = name;
+    throw error;
+  }
+};
+
+module.exports = exec;
