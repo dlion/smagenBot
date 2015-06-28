ae2d6f176774bf14d5965ce0c0754d6752141b40
Domenico Luciani <domenicoleoneluciani@gmail.com>
2015-06-28 16:18:26 +0200

    Catch Exception #4

--- index.js
+++ index.js
@@ -5,27 +5,31 @@ var bot = new smagenBot(config);
 
 function check() {
   bot.getUpdates(function (err, body) {
-    if (!err && body !== null) {
-      if (body.result.length > 0) {
-        //Watch the last message
-        var current = body.result[body.result.length-1];
-        //From my chat and nobody else
-        if(current.update_id >= bot.offset) {
-          bot.chatId = current.message.chat.id;
-          bot.offset = current.update_id + 1;
-          if(!bot.username) {
-            bot.makeAction(current.message.text);
-          } else {
-            if (bot.username.indexOf(current.message.from.username) !== -1) {
+    try {
+      if (!err && body !== null) {
+        if (body.result.length > 0) {
+          //Watch the last message
+          var current = body.result[body.result.length-1];
+          //From my chat and nobody else
+          if(current.update_id >= bot.offset) {
+            bot.chatId = current.message.chat.id;
+            bot.offset = current.update_id + 1;
+            if(!bot.username) {
               bot.makeAction(current.message.text);
             } else {
-              bot.sendMessage("Only "+JSON.stringify(config.username)+" can use me!");
+              if (bot.username.indexOf(current.message.from.username) !== -1) {
+                bot.makeAction(current.message.text);
+              } else {
+                bot.sendMessage("Only "+JSON.stringify(config.username)+" can use me!");
+              }
             }
           }
         }
+      } else {
+        throw new Error("Error on getUpdates");
       }
-    } else {
-      throw new Error("Error on getUpdates");
+    } catch(e) {
+      console.log(e.message);
     }
   });
 }
